1470a196fce41ede2c89e477d8a2028eede9010b
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-09-05 19:49:30 -0300

    fix:  error handling to use i18next for translation

--- src/core/middlewares/errorHandler.ts
+++ src/core/middlewares/errorHandler.ts
@@ -1,20 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpError } from '@/core/errors/http.errors';
+import { i18next } from '@/core/config/i18n.config';
 
 export function errorHandler(
   err: Error,
   req: Request,
   res: Response,
+  //disable-next-line eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next: NextFunction,
 ) {
-  console.log('Handling HttpError:', req.t);
   if (err instanceof HttpError) {
     const message = req.t(err.message, { ...err.params });
     return res.status(err.statusCode).json({ message });
   }
 
   if (err instanceof SyntaxError && 'body' in err) {
-    const message = req.t('errors.invalidBody');
+    const t = i18next.t.bind(i18next);
+
+    const message = t('errors.invalidBody');
     return res.status(400).json({ message });
   }
 
