742ef47906d989a3eb1f288ae8bb03a739bdab42
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-09-05 13:54:17 -0300

    test: add tests region search dtos

--- src/app/regionSearch/dtos/findByDistance.dto.ts
+++ src/app/regionSearch/dtos/findByDistance.dto.ts
@@ -2,5 +2,8 @@ import { z } from '@/core/config/zod.config';
 import { pointSchema } from './point.dto';
 
 export const findByDistanceSchema = pointSchema.extend({
-  distance: z.coerce.number().min(0).describe('Distance em metros'),
+  distance: z.coerce
+    .number()
+    .min(0, 'A distância deve ser um número positivo')
+    .describe('Distance em metros'),
 });
--- src/app/regionSearch/dtos/point.dto.ts
+++ src/app/regionSearch/dtos/point.dto.ts
@@ -1,8 +1,16 @@
 import { z } from '@/core/config/zod.config';
 
 export const pointSchema = z.object({
-  longitude: z.coerce.number().min(-180).max(180).describe('Longitude'),
-  latitude: z.coerce.number().min(-90).max(90).describe('Latitude'),
+  longitude: z.coerce
+    .number()
+    .min(-180, 'Longitude deve ser maior ou igual a -180')
+    .max(180, 'Longitude deve ser menor ou igual a 180')
+    .describe('Longitude'),
+  latitude: z.coerce
+    .number()
+    .min(-90, 'Latitude deve ser maior ou igual a -90')
+    .max(90, 'Latitude deve ser menor ou igual a 90')
+    .describe('Latitude'),
 });
 
 export type PointDto = z.infer<typeof pointSchema>;
