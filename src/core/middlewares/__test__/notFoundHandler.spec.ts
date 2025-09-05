681f09628398dcb80bd83812b83e6813672221ae
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-09-05 14:09:48 -0300

    test: add unit tests for notFoundHandler middleware

---
+++ src/core/middlewares/__test__/notFoundHandler.spec.ts
@@ -0,0 +1,25 @@
+import 'mocha';
+import { expect } from 'chai';
+import { Request, Response, NextFunction } from 'express';
+import { notFoundHandler } from '@/core/middlewares/notFoundHandler';
+import { HttpError } from '@/core/errors/http.errors';
+
+describe('notFoundHandler Middleware', () => {
+  it('deve chamar next com um HttpError de status 404', () => {
+    const req: Partial<Request> = {
+      originalUrl: '/fake-route',
+    };
+    const res: Partial<Response> = {};
+    let capturedError: any = null;
+
+    const next: NextFunction = (error) => {
+      capturedError = error;
+    };
+
+    notFoundHandler(req as Request, res as Response, next);
+
+    expect(capturedError).to.be.instanceOf(HttpError);
+    expect(capturedError.statusCode).to.equal(404);
+    expect(capturedError.message).to.equal("Rota '/fake-route' não encontrada");
+  });
+});
